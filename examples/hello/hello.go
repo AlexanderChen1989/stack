de2ef266e6e0b120b5bf5526a917048719b7f9d6
Alex Chen <alexanderchenmh@gmail.com>
2016-05-27 23:16:23 +0800

    add helper methods

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -6,17 +6,24 @@ import (
 
 	"github.com/AlexanderChen1989/plug"
 	"github.com/AlexanderChen1989/plug/plugs/requestid"
+	"github.com/AlexanderChen1989/plug/plugs/router/mux"
 )
 
 func main() {
 	b := plug.NewBuilder()
 
 	b.Plug(requestid.New())
-	b.Plug(requestid.NewWithHeader("My-Request-ID"))
 
-	b.Plug(plug.HandleFunc(func(conn plug.Conn) {
-		fmt.Fprintln(conn, "Hello, world!")
-	}))
+	router := mux.NewRouter()
+
+	router.DispatchFunc(
+		"/hello",
+		func(conn plug.Conn) {
+			fmt.Fprintln(conn, "Hello, world!")
+		},
+	)
+
+	b.Plug(router)
 
 	http.ListenAndServe(":8080", b.BuildHTTPHandler())
 }
--- plugs/router/mux/plug.go
+++ plugs/router/mux/plug.go
@@ -7,14 +7,27 @@ import (
 	"golang.org/x/net/context"
 )
 
+// Plug implements plug.Plugger.Plug
 func (r *Router) Plug(plug.Plugger) plug.Plugger {
 	return r
 }
 
+// HandleConn implements plug.Plugger.HandleConn
 func (r *Router) HandleConn(conn plug.Conn) {
 	r.ServeHTTP(conn.Context, conn.ResponseWriter, conn.Request)
 }
 
+// Dispatch dispatch request to  plug.Plugger
+func (r *Router) Dispatch(path string, p plug.Plugger) *Route {
+	return r.Handle(path, ToHandler(p))
+}
+
+// DispatchFunc dispatch request to func(conn plug.Conn)
+func (r *Router) DispatchFunc(path string, f func(conn plug.Conn)) *Route {
+	return r.Handle(path, ToHandlerFunc(f))
+}
+
+// ToHandler convert plug.Plugger to Handler
 func ToHandler(p plug.Plugger) Handler {
 	return HandlerFunc(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		p.HandleConn(plug.Conn{
@@ -23,4 +36,15 @@ func ToHandler(p plug.Plugger) Handler {
 			Request:        r,
 		})
 	})
+}
+
+// ToHandlerFunc convert func(conn plug.Conn) to HandlerFunc
+func ToHandlerFunc(f func(conn plug.Conn)) HandlerFunc {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		f(plug.Conn{
+			Context:        ctx,
+			ResponseWriter: w,
+			Request:        r,
+		})
+	}
 }
