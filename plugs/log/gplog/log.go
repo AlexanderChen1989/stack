7c7fc8cde83983cd3d8a3c90eafff48ad5454ed6
Alex Chen <alexanderchenmh@gmail.com>
2016-05-31 11:11:17 +0800

    add helper functions to use log plugger

--- plugs/log/gplog/log.go
+++ plugs/log/gplog/log.go
@@ -14,8 +14,8 @@ type logPlug struct {
 
 // Config config logger plugger
 type Config struct {
-	Levels  []log.Level
-	Backand log.Handler
+	Levels     []log.Level
+	LogHandler log.Handler
 }
 
 // EmptyHandler is handler for logger plug, which will discard all log message
@@ -35,18 +35,81 @@ func (empty EmptyHandler) Run() chan<- *log.Entry {
 // NewWitConfig create log plugger with config,
 // if config.Backand is nil, console handler will be used
 func NewWitConfig(cfg Config) plug.Plugger {
-	if cfg.Backand == nil {
-		cfg.Backand = console.New()
+	if cfg.LogHandler == nil {
+		cfg.LogHandler = console.New()
 	}
-	log.RegisterHandler(cfg.Backand, cfg.Levels...)
+	log.RegisterHandler(cfg.LogHandler, cfg.Levels...)
 	return &logPlug{FieldLeveledLogger: log.Logger}
 }
 
 // New create new log plugger
-func New() plug.Plugger {
-	return NewWitConfig(Config{
+func New(fns ...func(Config) Config) plug.Plugger {
+	cfg := Config{
 		Levels: log.AllLevels,
-	})
+	}
+	for _, fn := range fns {
+		cfg = fn(cfg)
+	}
+	if cfg.LogHandler == nil {
+		cfg.LogHandler = console.New()
+	}
+	return NewWitConfig(cfg)
+}
+
+var allLevels = []log.Level{
+	log.DebugLevel,
+	log.TraceLevel,
+	log.InfoLevel,
+	log.NoticeLevel,
+	log.WarnLevel,
+	log.ErrorLevel,
+	log.PanicLevel,
+	log.AlertLevel,
+	log.FatalLevel,
+}
+
+// Info log info level
+func Info() func(Config) Config {
+	return Level(log.InfoLevel)
+}
+
+// Debug log debug level
+func Debug() func(Config) Config {
+	return Level(log.DebugLevel)
+}
+
+// Warn log warn level
+func Warn() func(Config) Config {
+	return Level(log.WarnLevel)
+}
+
+// Error log error level
+func Error() func(Config) Config {
+	return Level(log.ErrorLevel)
+}
+
+// Level config log level
+func Level(l log.Level) func(Config) Config {
+	var ls []log.Level
+	for i, level := range allLevels {
+		if level == l {
+			ls = allLevels[i:]
+			break
+		}
+	}
+
+	return func(cfg Config) Config {
+		cfg.Levels = ls
+		return cfg
+	}
+}
+
+// LogHandler config log handler
+func LogHandler(h log.Handler) func(Config) Config {
+	return func(cfg Config) Config {
+		cfg.LogHandler = h
+		return cfg
+	}
 }
 
 func (l *logPlug) Plug(next plug.Plugger) plug.Plugger {
