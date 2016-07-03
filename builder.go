4691dc186bad54dc1a3ee33f52d5e4b4c33d407b
Alex Chen <alexanderchenmh@gmail.com>
2016-07-03 10:28:22 +0800

    add doc

--- builder.go
+++ builder.go
@@ -2,24 +2,29 @@ package plug
 
 import "net/http"
 
+// NewBuilder create new Builder
 func NewBuilder() *Builder {
 	return &Builder{}
 }
 
+// Builder is builder for Pipe
 type Builder struct {
 	plugs []PlugFunc
 }
 
+// Plug plug Plug to Pipe
 func (b *Builder) Plug(plugs ...Plug) {
 	for _, p := range plugs {
 		b.plugs = append(b.plugs, p.ServeHTTP)
 	}
 }
 
+// PlugFunc plug PlugFunc to Pipe
 func (b *Builder) PlugFunc(plugs ...PlugFunc) {
 	b.plugs = append(b.plugs, plugs...)
 }
 
+// Build build Pipe
 func (b *Builder) Build() http.Handler {
 	return newPipe(b.plugs...)
 }
