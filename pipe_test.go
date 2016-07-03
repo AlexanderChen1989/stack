35aaedbe60499c55886e00dbec14a107755d06af
Alex Chen <alexanderchenmh@gmail.com>
2016-07-03 10:12:09 +0800

    add pipe test

--- pipe_test.go
+++ pipe_test.go
@@ -1,9 +1,9 @@
 package plug
 
 import (
-	"fmt"
+	"context"
 	"net/http"
-	"strconv"
+	"reflect"
 	"testing"
 )
 
@@ -15,34 +15,45 @@ func (m *P) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerF
 	next(nil, nil)
 }
 
-func mgen(name string) PlugFunc {
-	if name != "" {
-		return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-			fmt.Println(name)
-			next(nil, nil)
-		}
-	}
-
+func mgen() PlugFunc {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		next(nil, nil)
+	}
+}
+
+func mgen2(t *testing.T, id int) PlugFunc {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+		for i := 0; i <= id; i++ {
+			v := r.Context().Value(i)
+			if v != nil && !reflect.DeepEqual(v, i) {
+				t.Errorf("PID: %d -> ID: %d\n", v, i)
+				return
+			}
+		}
+
+		ctx := context.WithValue(r.Context(), id, id)
+		next(nil, r.WithContext(ctx))
 	}
 }
 
 func TestPlugFunc(t *testing.T) {
 	builder := NewBuilder()
 
-	for i := 0; i < 10; i++ {
-		builder.PlugFunc(mgen(strconv.Itoa(i)))
+	for i := 0; i < 1000; i++ {
+		builder.PlugFunc(mgen2(t, i))
 	}
 
-	builder.Build().ServeHTTP(nil, nil)
+	r, _ := http.NewRequest("", "", nil)
+
+	builder.Build().ServeHTTP(nil, r)
 }
 
 func BenchmarkPlugFunc(b *testing.B) {
 	builder := NewBuilder()
 
 	for i := 0; i < 10; i++ {
-		builder.PlugFunc(mgen(""))
+		builder.PlugFunc(mgen())
 	}
 
 	h := builder.Build()
