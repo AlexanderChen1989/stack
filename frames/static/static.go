fff528eb75472050e9507a0dcf83e51315e7b162
Alex Chen <alexanderchenmh@gmail.com>
2016-07-08 17:42:56 +0800

    add static

--- frames/static/static.go
+++ frames/static/static.go
@@ -1,25 +1,23 @@
 package static
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/AlexanderChen1989/stack"
 )
 
+var Default = Static(Config{
+	At:   "public",
+	From: "static",
+})
+
 type Config struct {
-	At           string
-	From         string
-	Gzip         bool
-	Brotli       bool
-	Only         []string
-	OnlyMatching []string
-	CacheVSN     string
-	CacheEtg     string
-	Headers      map[string]string
+	At      string
+	From    string
+	Headers map[string]string
 }
 
 func (conf *Config) setup() {
@@ -27,103 +25,36 @@ func (conf *Config) setup() {
 		panic("Config.At or Config.From cant be empty")
 	}
 	conf.At = path.Join("/", conf.At)
-	conf.From = path.Join("/", conf.From)
-
-	for i, d := range conf.Only {
-		if strings.ContainsRune(d, '.') {
-			conf.Only[i] = path.Join("/", d)
-			continue
-		}
-		conf.Only[i] = path.Join("/", d) + "/"
+	from, err := filepath.Abs(conf.From)
+	if err != nil {
+		panic(err)
 	}
-
-	for i, d := range conf.OnlyMatching {
-		conf.OnlyMatching[i] = path.Join("/", d)
-	}
-
-	if conf.CacheVSN == "" {
-		conf.CacheVSN = "public, max-age=31536000"
-	}
-
-	if conf.CacheEtg == "" {
-		conf.CacheEtg = "public"
-	}
-
-	if conf.Headers == nil {
-		conf.Headers = map[string]string{}
-	}
+	conf.From = from
 }
 
-var Default = Static(&Config{
-	At:   "public",
-	From: "static",
-})
-
-func allow(conf *Config, r *http.Request) (string, bool) {
-	if r.Method != http.MethodGet && r.Method != http.MethodHead {
-		return "", false
-	}
-
-	cleaned := path.Clean(r.URL.Path)
-
-	if !strings.HasPrefix(cleaned, conf.At) {
-		return "", false
-	}
-
-	trimed := cleaned[len(conf.At):]
-
-	for _, onlyMatch := range conf.OnlyMatching {
-		if strings.HasPrefix(trimed, onlyMatch) {
-			return trimed, true
-		}
-	}
-
-	for _, only := range conf.Only {
-		if strings.HasPrefix(trimed, only) {
-			return trimed, true
-		}
-	}
-
-	return "", false
+func allow(conf *Config, r *http.Request) bool {
+	return (r.Method == http.MethodGet || r.Method == http.MethodHead) && strings.HasPrefix(r.URL.Path, conf.At)
 }
 
-func fileEncode(conf *Config, file string) (string, os.FileInfo, error) {
-	if conf.Brotli {
-		path := file + ".br"
-		info, err := os.Stat(path)
-		if err == nil {
-			return path, info, nil
-		}
-	}
+func Static(conf Config) stack.FrameFunc {
+	conf.setup()
 
-	if conf.Gzip {
-		path := file + ".gzip"
-		info, err := os.Stat(path)
-		if err == nil {
-			return path, info, nil
-		}
-	}
+	h := http.StripPrefix(
+		conf.At,
+		http.FileServer(
+			http.Dir(conf.From),
+		),
+	)
 
-	info, err := os.Stat(file)
-	return file, info, err
-}
-
-func Static(conf *Config) stack.FrameFunc {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		trimed, allowed := allow(conf, r)
-
-		if !allowed {
-			next(w, r)
+		if allow(&conf, r) {
+			for k, v := range conf.Headers {
+				w.Header().Add(k, v)
+			}
+			h.ServeHTTP(w, r)
 			return
 		}
 
-		file, info, err := fileEncode(conf, path.Join(conf.From, trimed))
-		if err != nil {
-			next(w, r)
-			return
-		}
-		// add cache
-
-		fmt.Println(file, info, err)
+		next(w, r)
 	}
 }
--- frames/static/static_test.go
+++ frames/static/static_test.go
@@ -1,41 +1,39 @@
 package static
 
 import (
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/AlexanderChen1989/stack"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConfig(t *testing.T) {
-	conf := &Config{
-		At:           "public",
-		From:         "static",
-		Only:         []string{"images", "js", "favicon.co"},
-		OnlyMatching: []string{"css"},
+func TestStatic(t *testing.T) {
+	headers := map[string]string{
+		"X-Test": "Hello, world!",
 	}
-	conf.setup()
 
-	// test allow
-	paths := []struct {
-		path    string
-		trimed  string
-		matched bool
-	}{
-		{"/public/images/hello.png", "/images/hello.png", true},
-		{"/public/images-hello.png", "/images/hello.png", false},
-		{"/public/favicon.co", "/favicon.co", true},
-		{"/public/css-style.css", "/css-style.css", true},
-		{"/public/css/style.css", "/css/style.css", true},
-		{"/public/../../css/style.css", "/css/style.css", false},
-		{"/public/css/././style.css", "/css/style.css", true},
+	fn := Static(Config{
+		From:    "./test_static",
+		At:      "public",
+		Headers: headers,
+	})
+
+	b := stack.NewBuilder()
+	b.PushFunc(fn)
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/public/style.css", nil)
+	b.Build().ServeHTTP(w, r)
+
+	for k, v := range headers {
+		assert.Equal(t, v, w.Header().Get(k))
 	}
-	for _, p := range paths {
-		r, _ := http.NewRequest("GET", p.path, nil)
-		trimed, matched := allow(conf, r)
-		assert.Equal(t, p.matched, matched)
-		if p.matched {
-			assert.Equal(t, p.trimed, trimed)
-		}
-	}
+
+	data, err := ioutil.ReadAll(w.Body)
+	assert.Nil(t, err)
+	data2, err := ioutil.ReadFile("./test_static/style.css")
+	assert.Nil(t, err)
+	assert.Equal(t, data, data2)
 }
